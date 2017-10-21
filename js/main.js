ce9bfd658eea17703f4814e0ed5d2bdee4a3ecf6
Felipe Tascón <fstascon@gmail.com>
2017-10-21 10:28:09 +0200

    trigger btns and toggle elements

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 $(function() {
+  /*elements*/
   elements = new Array("login", "register", "lostpassword");
+  /*check url*/
   chk(elements);
-
-});
-function change(ctm_active, ctm_elements) {
-  ctm_elements.map(function(){
-    
+  /*trigger clicks with btn_ */
+  $('.flex .box + p a').on("click",function() {
+    change($(this).attr("class"), elements);
   });
+});
+
+/* function change */
+function change(btn_active, ctm_elements) {
+  ctm_elements.map(function(ele){
+    $('.'+ele).hide();
+    $('.btn_'+ele).show();
+  });
+  let ele_active = btn_active.substr(4);
+  $('.'+btn_active).hide();
+  $('.'+ele_active).show();
 }
+/*function url*/
 function chk(ctm_elements) {
   var i = 0;
   ctm_elements.map(function(ele){
